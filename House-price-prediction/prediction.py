aae3eb68e3599e981de4195af5b3f2d945b9ff13
Vansh Mhatre <vanshmhatre342006@gmail.com>
2026-03-05 23:26:25 +0530

    feat : modified prediction file

--- House-price-prediction/prediction.py
+++ House-price-prediction/prediction.py
@@ -8,9 +8,9 @@ from model import House_price
 x_scaler = joblib.load("House-price-prediction/saved_models/x_scaler.joblib")
 y_scaler = joblib.load("House-price-prediction/saved_models/y_scaler.joblib")
 
-model = House_price(16 )
+model = House_price(16)
 
-model.load_state_dict(torch.load('House-price-prediction/saved_models/trained_model_01.pth'))
+model.load_state_dict(torch.load('House-price-prediction/saved_models/trained_model_02.pth'))
 
 model.eval()
 
@@ -29,9 +29,15 @@ def prediction(model ,X ,x_scaler ,y_scaler):
     pred =y_scaler.inverse_transform(pred.detach().numpy()).squeeze()
     pred = np.expm1(pred)
     pred = pred/100000
-    print(f"{pred}:.2f lakhs")
+    print(f"{pred : .2f} lakhs")
     
     return pred
     
 X = [3,1,900,4770,1,0,3,6,900,0,1969,2009,900,3480,2,55]
-output =prediction(model,X ,x_scaler,y_scaler)
+X1=[3,1.75,1820,3140,2,0,5,8,1820,0,1949,1990,2030,5499,1,55]
+X2 =[4,2.5,3310,42998,2,0,3,9,3310,0,2001,0,3350,42847,3,76]
+
+output =prediction(model,X2 ,x_scaler,y_scaler)
+
+
+
